fe839c96ed24d804aa7f01737e562940053e16ca
Raidanor <silverstallion30@gmail.com>
2024-08-16 21:20:57 -0300

    16 Aout 2024

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,10 +1,36 @@
+import Conversation from "../models/conversation.model.js"
+
 export const sendMessage = async (req, res) =>
 {
     try
     {
         const { message } = req.body
         const { id } = req.params
-        const { senderId } = req.userId
+        const { senderId } = req.user._Id
+
+        let conversation = await Conversation.findOne({
+            participants: { $all: [senderId, receiverId]},
+        })
+
+        if (!conversation) {
+            consversation = await Conversation.create({
+                participant: [senderId, receiverId],
+
+            })
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message
+        })
+
+        if (newMessage)
+        {
+            conversation.messages.push(newMessage._id)
+        }
+
+        res.status(201).json( newMessage )
     }
     catch (error)
     {
--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ const app  = express();
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 
