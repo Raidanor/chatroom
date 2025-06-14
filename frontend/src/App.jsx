8aae5f6676d486484bef078a4d64358b9d92a4ed
Raidanor <silverstallion30@gmail.com>
2025-06-14 17:05:37 -0300

    added signup hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,22 @@ import { useState } from 'react'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import Home from './pages/Home.jsx'
+import { Routes, Route } from 'react-router-dom'
+
+import { Toaster } from "react-hot-toast"
 
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-        <Home />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+
+        </Routes>
+        <Toaster />
     </div>
   )
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 // import { Link } from "react-router-dom";
 // import useLogin from "../../hooks/useLogin";
 
@@ -6,7 +7,7 @@ import { useState } from "react";
 const Login = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-clip-padding backdrop-filter backdrop-blur-lg'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Login
 					<span className='text-blue-500'> ChatApp</span>
@@ -30,9 +31,9 @@ const Login = () => {
 							className='w-full input input-bordered h-10'
 						/>
 					</div>
-					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
 						{"Don't"} have an account?
-					</a>
+					</Link>
 
 					<div>
 						<button className='btn btn-block btn-sm mt-2'>Login</button>
