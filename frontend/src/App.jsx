66666d70264e3fc03d538903d644b9c7decebaf5
Raidanor <silverstallion30@gmail.com>
2025-06-19 17:03:18 -0300

    added signup functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,24 +2,26 @@ import { useState } from 'react'
 import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import Home from './pages/Home.jsx'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { Toaster } from "react-hot-toast"
+import { useAuthContext } from './context/AuthContext.jsx'
 
 
 function App() {
+    const {authUser} = useAuthContext()
 
-  return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+    return (
+        <div className='p-4 h-screen flex items-center justify-center'>
+            <Routes>
+                <Route path="/" element={!authUser ? <Navigate to="/login" /> : <Home />} />
+                <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+                <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
 
-        </Routes>
-        <Toaster />
-    </div>
-  )
+            </Routes>
+            <Toaster />
+        </div>
+    )
 }
 
 export default App
