d43f5c28aff3b3bd7091b31f0174656b1977a4cd
Raidanor <silverstallion30@gmail.com>
2025-06-21 20:21:00 -0300

    added send message functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,17 +1,25 @@
 import MessageInput from "./MessageInput"
 import Messages from "./Messages"
+import useConversation from '../../zustand/useConversation'
+
 import { TiMessages } from "react-icons/ti"
+import { useEffect } from "react"
 
 const MessageContainer = () => {
-    const noChatselected = true
+    const {selectedConversation, setSelectedConversation} = useConversation()
+
+    useEffect(() => {
+        //cleanup on unmount
+        return () => setSelectedConversation(null)
+    }, [])
     return (
         <div className='md:min-w-[450px] flex-col flex'>
-            {noChatselected ? <NoChatSelected /> : 
+            {!selectedConversation ? <NoChatSelected /> : 
                 <>
                     {/* Header */}
                     <div className='bg-slate-500 px-4 py-2 mb-2'>
                         <span className="label-text">To:</span>
-                        <span className='text-gray-900 font-bold'>Username</span>
+                        <span className='text-gray-900 font-bold'>{selectedConversation.fullname}</span>
                     </div>
 
                     <Messages />
--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -4,10 +4,11 @@ import useConversation from '../../zustand/useConversation'
 const Conversation = ({conversation, lastIdx}) => {
     const {selectedConversation, setSelectedConversation} = useConversation()
     const isSelected = selectedConversation?._id === conversation._id
+
     return (
         <>
             <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-                ${isSelected} ? "bg-sky-500" : ""`} onClick={() => setSelectedConversation(conversation)}>
+                ${isSelected ? "bg-sky-700" : ""}`} onClick={() => setSelectedConversation(conversation)}>
                 <div className='avatar online'>
                     <div className='w-12 rounded-full'>
                         <img
