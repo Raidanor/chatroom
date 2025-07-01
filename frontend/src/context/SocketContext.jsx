f979f9fb7c6b50b54bb7dcb8e7494bd986269cb8
Raidanor <silverstallion30@gmail.com>
2025-07-01 03:09:06 -0300

    final changes

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children}) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chatroom-w76u.onrender.com", {
                 query: {
                     userId: authUser._id
                 }
